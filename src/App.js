a2b5540fb720567c9d0a513b3c937f561fbd4380
Niiazbek Mamasaliev <niyazbek.mamasaliev@gmail.com>
2023-03-17 04:27:21 +0100

    route is added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import NotesListPage from "./pages/NotesListPage";
+import NotePage from "./pages/NotePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<NotesListPage />} />
+          <Route path="/notes/:id" element={<NotePage />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
